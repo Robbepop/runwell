543d3c3244899765fd1fb373b3932cad36bb4613
Robin Freyler <robin.freyler@gmail.com>
2019-12-14 17:31:39 +0100

    add ir::Function::entry_block accessor

--- src/ir/function.rs
+++ src/ir/function.rs
@@ -8,8 +8,17 @@ pub struct Function {
     locals: FunctionLocals,
     /// The non-empty set of basic blocks that form the operations
     /// performance by the function seen as a whole.
-    /// The first basic block is special in that it is executed directly
-    /// upon executing the function itself. Also it must not have precedessors
-    /// which implies that it cannot contain PHI operations.
+    /// The first basic block (entry block) is special in that it is executed
+    /// directly upon executing the function itself.
+    /// Also it must not have precedessors which implies
+    /// that it cannot contain PHI operations.
     blocks: Vec<BasicBlock>,
+}
+
+impl Function {
+    /// Returns the entry block of the function.
+    pub fn entry_block(&self) -> &BasicBlock {
+        // This can never fail since `blocks` is guaranteed to be non-empty.
+        &self.blocks[0]
+    }
 }
