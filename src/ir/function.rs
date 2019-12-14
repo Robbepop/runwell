8883e84113c5f6b6d2e5a12bebdf018d5bf9e04b
Robin Freyler <robin.freyler@gmail.com>
2019-12-14 17:54:11 +0100

    improve ir::Function docs slightly

--- src/ir/function.rs
+++ src/ir/function.rs
@@ -3,8 +3,11 @@
 pub struct TypeId(usize);
 
 pub struct Function {
+    /// The globally unique identifier of the function.
     id: FunctionId,
+    /// The signature of the function.
     signature: FunctionSig,
+    /// The used local variable declarations of the function.
     locals: FunctionLocals,
     /// The non-empty set of basic blocks that form the operations
     /// performance by the function seen as a whole.
