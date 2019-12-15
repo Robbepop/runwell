6d24f84b56fe9109688c60f5abcdf289e3e881c6
Robin Freyler <robin.freyler@gmail.com>
2019-12-15 16:50:52 +0100

    make maybe_std submodule private

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ extern crate test;
 extern crate alloc;
 
 pub mod ir;
-pub mod maybe_std;
+mod maybe_std;
 pub mod parse;
 
 // Function 184 of type FuncType { form: Func, params: [I32, I32, I32], returns: [I32] }
