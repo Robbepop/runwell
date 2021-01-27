f00b9b1ccb9891626d1924d888c358b4a73d4fd1
Robin Freyler <robin.freyler@gmail.com>
2021-01-28 00:34:20 +0100

    add module doc for ir module

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! Runwell IR data structures, SSA builder and IR interpreter.
+
 mod builder;
 mod error;
 mod instruction;
