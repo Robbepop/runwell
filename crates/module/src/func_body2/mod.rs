d98f49f77b0af440f6b8d0fc651ff8da897bcc0e
Robin Freyler <robin.freyler@gmail.com>
2021-03-07 22:36:25 +0100

    implement initial skeleton of the new FunctionBody type

--- crates/module/src/func_body2/mod.rs
+++ crates/module/src/func_body2/mod.rs
@@ -20,14 +20,106 @@ mod instruction;
 mod value;
 mod variable;
 
-pub use self::error::{FunctionBuilderError, VariableAccess};
-use self::{
-    builder::{FunctionBuilder, FunctionBuilderState},
+pub use self::{
+    builder::FunctionBuilder,
+    error::{FunctionBuilderError, VariableAccess},
     instruction::InstructionBuilder,
+};
+use self::{
+    builder::FunctionBuilderState,
     value::{ValueDefinition, ValueUser},
     variable::{Variable, VariableTranslator},
 };
+use crate::{module::Indent, primitive::Instr};
+use entity::{
+    ComponentVec,
+    DefaultComponentMap,
+    DefaultComponentVec,
+    EntityArena,
+    PhantomEntityArena,
+    RawIdx,
+};
+use ir::{
+    instr::Instruction,
+    primitive::{
+        Block,
+        BlockEntity,
+        DisplayEdge,
+        Edge,
+        EdgeEntity,
+        Type,
+        Value,
+        ValueEntity,
+    },
+};
+use smallvec::SmallVec;
 
 /// A virtual, verified Runwell IR function.
 #[derive(Debug)]
-pub struct FunctionBody {}
+pub struct FunctionBody {
+    /// Arena for all block entities.
+    blocks: PhantomEntityArena<BlockEntity>,
+    /// Arena for all SSA value entities.
+    values: PhantomEntityArena<ValueEntity>,
+    /// Arena for all branching edge entities.
+    edges: PhantomEntityArena<EdgeEntity>,
+    /// Arena for all IR instructions.
+    instrs: EntityArena<Instruction>,
+    /// The basic block value parameters.
+    block_params: DefaultComponentVec<Block, SmallVec<[Value; 4]>>,
+    /// Block instructions.
+    ///
+    /// # Note
+    ///
+    /// Also contains all the phi instructions at the block start.
+    block_instrs: DefaultComponentVec<Block, SmallVec<[Instr; 4]>>,
+    /// The arguments for the destination block parameters.
+    edge_args: DefaultComponentVec<Edge, SmallVec<[Value; 4]>>,
+    /// The destination basic block of the edge.
+    edge_destination: ComponentVec<Edge, Block>,
+    /// Return values of the instruction.
+    ///
+    /// Not all instructions can be associated with an SSA value.
+    /// For example `store` is not in pure SSA form and therefore
+    /// has no SSA value association.
+    instr_values: DefaultComponentMap<Instr, SmallVec<[Option<Value>; 4]>>,
+    /// Types for all values.
+    value_type: ComponentVec<Value, Type>,
+    /// The association of the SSA value.
+    ///
+    /// Every SSA value has an association to either an IR instruction
+    /// or to an input parameter of the IR function under construction.
+    value_definition: ComponentVec<Value, ValueDefinition>,
+}
+
+impl FunctionBody {
+    /// Returns the entry block of the function.
+    pub fn entry_block(&self) -> Block {
+        Block::from_raw(RawIdx::from_u32(0))
+    }
+
+    /// Returns the maximum SSA value used by the function.
+    pub fn max_value(&self) -> Value {
+        self.values
+            .indices()
+            .last()
+            .unwrap_or_else(|| Value::from_raw(RawIdx::from_u32(0)))
+    }
+
+    /// Returns the slice over the output values of the instruction.
+    fn instr_values(&self, instr: Instr) -> &[Option<Value>] {
+        self.instr_values[instr].as_slice()
+    }
+
+    /// Returns the nth instruction of the block and its assoc value if any.
+    pub fn instruction_and_value(
+        &self,
+        block: Block,
+        n: usize,
+    ) -> Option<(&[Option<Value>], &Instruction)> {
+        let instr = self.block_instrs[block].get(n).copied()?;
+        let instruction = &self.instrs[instr];
+        let instr_values = self.instr_values(instr);
+        Some((instr_values, instruction))
+    }
+}
