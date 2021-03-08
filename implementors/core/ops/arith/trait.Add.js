c6a748604cdbbdbe589e59e4b950518adffe7f43
Robbepop <Robbepop@users.noreply.github.com>
2021-03-08 16:17:50 +0000

    deploy: 8e833169e754a12feb56f0f5587469d2b02977e7

---
+++ implementors/core/ops/arith/trait.Add.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["runwell_ir"] = [{"text":"impl Add&lt;Indent&gt; for Indent","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
