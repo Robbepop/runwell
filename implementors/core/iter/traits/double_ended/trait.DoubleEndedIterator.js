29b1353a5aed8dea715a6126de864e23257c69bd
Robbepop <Robbepop@users.noreply.github.com>
2021-03-11 23:01:06 +0000

    deploy: 6cefc48f44e4cbd267a3899a6603797efacc6564

--- implementors/core/iter/traits/double_ended/trait.DoubleEndedIterator.js
+++ implementors/core/iter/traits/double_ended/trait.DoubleEndedIterator.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
 implementors["runwell_entity"] = [{"text":"impl&lt;'a, T&gt; DoubleEndedIterator for Indices&lt;'a, T&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a, T&gt; DoubleEndedIterator for Entities&lt;'a, T&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a, T&gt; DoubleEndedIterator for EntitiesMut&lt;'a, T&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a, T&gt; DoubleEndedIterator for Iter&lt;'a, T&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a, T&gt; DoubleEndedIterator for IterMut&lt;'a, T&gt;","synthetic":false,"types":[]}];
+implementors["runwell_ir"] = [{"text":"impl&lt;'a&gt; DoubleEndedIterator for MatchSelectResultsIter&lt;'a&gt;","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ runwell_ir/instr/utils/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["MatchSelectInstrBuilder","Builder used to incrementally build up a multi-value returning match select instruction."],["MatchSelectResultsIter","Iterator over the result values of all match arms of a `MatchSelectInstr`."]]});
