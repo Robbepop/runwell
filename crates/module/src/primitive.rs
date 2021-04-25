4a7455728174199032d50260f5754b87b64afb73
Robin Freyler <robin.freyler@gmail.com>
2021-04-25 11:05:23 +0200

    add primitive module docs

--- crates/module/src/primitive.rs
+++ crates/module/src/primitive.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! Primitive Wasm types that implement conversion to Runwell types.
+
 pub use super::{
     func_body::{Instr, Variable},
     func_type::FunctionType,
