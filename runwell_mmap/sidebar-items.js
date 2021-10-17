9893fffe4e3ac33200192649112fa82be76b9fc8
Robbepop <Robbepop@users.noreply.github.com>
2021-10-17 14:59:10 +0000

    deploy: a20a99420867ec3809be3568c58c4bd50fb0ccbe

--- runwell_mmap/sidebar-items.js
+++ runwell_mmap/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Error","A collection of possible errors."]],"struct":[["VirtualMemory","A virtually allocated memory."]],"type":[["Result","The result type used by this library."]]});
+initSidebarItems({"enum":[["Error","An error that may occur upon operating with virtual memory."]],"struct":[["VirtualMemory","A virtually allocated memory."]]});
