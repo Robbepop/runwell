28d22c021751def13f90cbc88011cf5fe5fce5ca
Robbepop <Robbepop@users.noreply.github.com>
2021-11-21 13:35:26 +0000

    deploy: 223f709331500d4af057d48e3a9ee418e20bcba2

--- runwell_vmem/sidebar-items.js
+++ runwell_vmem/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Error","An error that may occur upon operating with virtual memory."]],"struct":[["VirtualMemory","A virtually allocated memory."]]});
+initSidebarItems({"enum":[["Error","An error that may occur upon operating with virtual memory."]],"struct":[["RegionError","An error originating from the `region` crate."],["VirtualMemory","A virtually allocated memory."]]});
