2eb94b0d9e8523791013cfd082524f292e1d1a3b
Robin Freyler <robin.freyler@gmail.com>
2021-11-21 14:33:21 +0100

    wrap region::Error in RegionError
    This allows to implement some missing traits, such as PartialEq.

--- crates/vmem/src/lib.rs
+++ crates/vmem/src/lib.rs
@@ -26,14 +26,67 @@ use core::{
     slice::{self, SliceIndex},
 };
 
-/// An error that may occur upon operating with virtual memory.
+/// An error originating from the `region` crate.
 #[derive(Debug)]
+pub struct RegionError(region::Error);
+
+impl Display for RegionError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        fmt::Display::fmt(&self.0, f)
+    }
+}
+
+impl From<region::Error> for RegionError {
+    fn from(error: region::Error) -> Self {
+        Self(error)
+    }
+}
+
+#[cfg(
+    // Comparing virtual memories is a very costly operation
+    // that should not be supported or used outside of test code.
+    test
+)]
+impl PartialEq for RegionError {
+    fn eq(&self, other: &Self) -> bool {
+        match (&self.0, &other.0) {
+            (region::Error::UnmappedRegion, region::Error::UnmappedRegion) => {
+                true
+            }
+            (
+                region::Error::InvalidParameter(lhs),
+                region::Error::InvalidParameter(rhs),
+            ) => lhs == rhs,
+            (
+                region::Error::ProcfsInput(lhs),
+                region::Error::ProcfsInput(rhs),
+            ) => lhs == rhs,
+            (
+                region::Error::SystemCall(lhs),
+                region::Error::SystemCall(rhs),
+            ) => lhs.kind() == rhs.kind(),
+            (region::Error::MachCall(lhs), region::Error::MachCall(rhs)) => {
+                lhs == rhs
+            }
+            _ => false,
+        }
+    }
+}
+
+/// An error that may occur upon operating with virtual memory.
+#[derive(Debug, PartialEq)]
 pub enum Error {
-    Region(region::Error),
+    Region(RegionError),
 }
 
 impl From<region::Error> for Error {
     fn from(error: region::Error) -> Self {
+        Self::Region(RegionError::from(error))
+    }
+}
+
+impl From<RegionError> for Error {
+    fn from(error: RegionError) -> Self {
         Self::Region(error)
     }
 }
